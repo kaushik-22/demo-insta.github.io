8aba05e6ef78579814959be95df71c3ee93a0e43
kaushik-22 <kaushikr@Kaushiks-MacBook-Pro.local>
2022-12-27 15:54:10 +0530

    blog init code

---
+++ index.php
@@ -0,0 +1,12 @@
+?php
+	// get files in the current directory
+	$files = scandir( getcwd() );
+?>
+<!DOOCTYPE html>
+<html>
+    <head>
+        <title>
+            Kaushik testing Graph API
+        </title>
+    </head>
+</html>
